7afe92a951083e631cbad121795ff8471b32aded
Chris Terrazas <chris.terrazas@gmail.com>
2019-04-04 17:58:07 -0400

    added modules to app, added some css for them

--- bike_tracker/src/App.js
+++ bike_tracker/src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="app">
+        <h1>Bike Tracker</h1>
+        <section className="module"><h2>current status</h2></section>
+        <section className="module"><h2>status</h2></section>
+        <section className="module"><h2>track</h2></section>
+        <section className="module"><h2>ride</h2></section>
       </div>
     );
   }
