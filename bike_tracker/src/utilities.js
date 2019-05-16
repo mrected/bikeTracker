a2264971e885e5cf18f447d8b6a0be5c55213d81
Chris Terrazas <chris.terrazas@gmail.com>
2019-05-16 13:34:32 -0400

    gets user miles per week and figures remaining days left to replace item

--- bike_tracker/src/utilities.js
+++ bike_tracker/src/utilities.js
@@ -1,3 +1,13 @@
+const getStatusColor = (status) => {
+    if(status <= 80){
+        return 'good'
+    } else if( status > 80 && status <= 100){
+        return 'warning'
+    } else{
+        return 'danger'
+    }
+}
+
 
 
 const submitBtn = document.getElementById('submitBtn')
@@ -40,15 +50,28 @@ submitBtn.addEventListener('click', (e) => {
     remainingMiles = totalMiles - currentMileageOnItem
     const remainingPercentage = Math.round((currentMileageOnItem / totalMiles) * 100)
     
-    if(remainingPercentage <= 80){
-        statusText = 'good'
-    } else if( remainingPercentage > 80 && remainingPercentage <= 100){
-        statusText = 'warning'
-    } else{
-        statusText = 'danger'
-    }
+    statusText = getStatusColor(remainingPercentage)
     
     displayString = `<p>Of ${totalMiles} total miles, you have ${remainingMiles} left until you have to replace this.</p>`
+    displayString += `<p>You have put ${currentMileageOnItem} on this so far</p>`
     displayString += `<p>The current condition of this is <span style="color: ${statusColor[statusText]}">${statusText}</span></p>`
     display.innerHTML = displayString
+
+    // get todays date
+    const today = new Date();
+    // get the number of miles they'll go in a week
+    const estNumberOfMilesPerWeek = document.getElementById('userMiles').value
+    // divide that by 7 to get numberOfMilesPerDay
+    const numberOfMilesPerDay = Math.round(estNumberOfMilesPerWeek / 7)
+    // divide remaining miles by numberOfMilesPerDay to get numberOfDaysToReplacement
+    const numberOfDaysToReplacement = Math.round(remainingMiles / numberOfMilesPerDay)
+    // add numberOfDaysToReplacement to today to get estTimeToReplace
+    const estReplacementDate = new Date()
+    estReplacementDate.setDate(today.getDate() + numberOfDaysToReplacement)
+
+    console.log(`user miles per week: ${estNumberOfMilesPerWeek}\n 
+                number of miles per day: ${numberOfMilesPerDay}\n
+                days to replacement: ${numberOfDaysToReplacement}\n
+                today: ${today}\n
+                date with days added: ${estReplacementDate}`)
 })
